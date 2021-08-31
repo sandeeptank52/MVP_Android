88744ed4c4d573e09f63570672225308a1ce7b25
Trần Hải Sơn <haison250199@gmail.com>
2021-08-31 03:28:00 +0000

    Science section

--- app/src/main/java/com/application/bmiobesity/view/mainActivity/MainActivity.kt
+++ app/src/main/java/com/application/bmiobesity/view/mainActivity/MainActivity.kt
@@ -50,17 +50,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
     private lateinit var mGoogleSignInService: GoogleSignInService
 
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = MainActivityV2Binding.inflate(layoutInflater)
         setTheme(R.style.Theme_DiseaseTrackerProductionCustom)
         setContentView(mainBinding.root)
-         eventManager.getPreloadSuccessEvent().observe(this, EventObserver{
-            if (it) mainBinding.mainFrameLayoutWaiting.visibility = View.GONE
-        })
 
         eventManager.getPreloadSuccessEvent().observe(this, EventObserver{
             if (it) mainBinding.mainFrameLayoutWaiting.visibility = View.GONE
@@ -117,6 +111,7 @@ class MainActivity : AppCompatActivity() {
                 } else {
                     IndicatorUiUpdate(R.id.mainHomeNav)
                     mainBinding.mainBottomNavigationView.visibility = View.VISIBLE
+                    mainBinding.mainMenu.visibility = View.VISIBLE
                     mainBinding.mainImageViewAvatarIcon.visibility = View.GONE
                     mainBinding.mainEnterText.visibility = View.GONE
                     mainBinding.mainForMoreAccurrate.visibility = View.GONE
@@ -130,9 +125,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-
-
     private fun initMainMenu(){
         mainBinding.mainMenu.setOnClickListener {
             val menu = PopupMenu(applicationContext, it)
@@ -140,6 +132,7 @@ class MainActivity : AppCompatActivity() {
             menu.setOnMenuItemClickListener{ menuItem ->
                 when (menuItem.itemId){
                     R.id.mainAppMenuSubs -> {subsMenuAction()}
+                    R.id.mainAppMenuScience -> {scienceMenuAction()}
                     R.id.mainAppMenuSetting -> {settingMenuAction()}
                     R.id.mainAppMenuLogOut -> {logOutMenuAction()}
                 }
@@ -155,6 +148,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun subsMenuAction(){
         navController.navigate(R.id.mainNavToSubs)
+    }
+    private fun scienceMenuAction() {
+        navController.navigate(R.id.mainNavToScience)
     }
     private fun settingMenuAction(){
         navController.navigate(R.id.mainNavToSetting)
