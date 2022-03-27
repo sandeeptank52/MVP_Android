846bc4be6b6c280b450ef5d34d0d38cbe5438cb2
Sandeep Tak <sandeeptank52@gmail.com>
2022-03-27 15:37:37 +0530

    Updated favorite UI as per new UI.

--- app/src/main/java/com/application/bmiobesity/view/mainActivity/home/favorite/HomeFavoriteAdapterRecycler.kt
+++ app/src/main/java/com/application/bmiobesity/view/mainActivity/home/favorite/HomeFavoriteAdapterRecycler.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -13,6 +16,7 @@ import com.application.bmiobesity.InTimeApp
 import com.application.bmiobesity.R
 import com.application.bmiobesity.model.db.paramSettings.entities.ResultCard
 import com.application.bmiobesity.utils.getFormatData
+import kotlinx.android.synthetic.main.main_home_favorite_card_view.view.*
 
 class HomeFavoriteAdapterRecycler(
     private val onClick: (ResultCard) -> Unit
@@ -44,14 +48,16 @@ class HomeFavoriteAdapterRecycler(
         private val favoriteCardTitle: TextView = itemView.findViewById(R.id.favoriteCardViewTitle)
         private val favoriteCardValue: TextView = itemView.findViewById(R.id.favoriteCardViewValue)
         private val favoriteCardDate: TextView = itemView.findViewById(R.id.favoriteCardViewData)
+        private val favoriteCardDetail : TextView = itemView.findViewById(R.id.favoriteCardViewDetails)
+        private val favoriteCardDetailView :ConstraintLayout = itemView.findViewById(R.id.favoriteCardViewDetailView)
         private var currentCard: ResultCard? = null
 
         init {
-            itemView.setOnClickListener {
-                currentCard?.let {
-                    onClick(it)
-                }
-            }
+//            itemView.setOnClickListener {
+//                currentCard?.let {
+//                    onClick(it)
+//                }
+//            }
         }
 
         fun bind(card: ResultCard) {
@@ -77,7 +83,9 @@ class HomeFavoriteAdapterRecycler(
             favoriteCardImage.setImageResource(idImage)
             favoriteCardTitle.setText(idTitle)
             favoriteCardDate.text = getFormatData("dd.MM.yyyy")
-
+            if(card.description.isNotEmpty()) {
+                favoriteCardDetail.text = card.description
+            }
             val resultValue = card.value
             val resultColor = card.valueColour
 
@@ -101,6 +109,13 @@ class HomeFavoriteAdapterRecycler(
             } else {
                 favoriteCardValue.setText(idErrorNotAvailable)
                 //favoriteCardValue.setTextColor(Color.RED)
+            }
+            itemView.setOnClickListener {
+                if(it.favoriteCardViewDetailView.isVisible){
+                    it.favoriteCardViewDetailView.visibility = View.GONE
+                }else{
+                    it.favoriteCardViewDetailView.visibility = View.VISIBLE
+                }
             }
         }
     }
